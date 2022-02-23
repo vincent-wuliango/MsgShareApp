dec0e6569c8e9b13abf393e25a21294bcbf4c326
SYeN <vwuliango@gmail.com>
2022-02-23 18:39:20 +0700

    Send message to other Activity, Share message to other Apps

--- app/src/main/java/com/xosyen/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/xosyen/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.xosyen.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,8 +8,7 @@ import android.widget.Toast
 import com.xosyen.msgshareapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +18,28 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnShowToast.setOnClickListener {
             Log.i("MainActivity", "Button was clicked!")
-
             Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = binding.etUserMessage.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
+
+            startActivity(intent)
+        }
+
+        binding.btnShareToOtherApps.setOnClickListener {
+            val message: String = binding.etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share to: "))
+
         }
     }
 }
---
+++ app/src/main/java/com/xosyen/msgshareapp/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.xosyen.msgshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.xosyen.msgshareapp.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bundle: Bundle? = intent.extras
+        val msg = bundle!!.getString("user_message")
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        binding.txtUserMessage.text = msg
+    }
+}
